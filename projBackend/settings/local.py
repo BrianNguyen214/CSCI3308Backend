d622abac91d7fc7db8d784b8bce4f10f2fb1a609
BrianNguyen214 <brng4374@colorado.edu>
2018-12-18 18:19:57 -0800

    initial commit

--- projBackend/settings/local.py
+++ projBackend/settings/local.py
@@ -78,11 +78,19 @@ WSGI_APPLICATION = 'projBackend.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    
 }
+
+# add this
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+#DATABASES['default']['CONN_MAX_AGE'] = 500
 
 
 # Password validation
